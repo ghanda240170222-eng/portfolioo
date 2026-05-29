4c26adfb91ac3bd0af8653a7a5c5d5875dc80c2e
ghanda240170222-eng <ghanda.240170222@mhs.unimal.ac.id>
2026-05-30 01:13:43 +0700

    fix: gunakan link googleusercontent resmi untuk foto profil

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -200,7 +200,7 @@
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="grid grid-cols-1 lg:grid-cols-12 gap-12 lg:gap-16 items-center">
                 
- <div class="lg:col-span-4 relative reveal flex justify-center lg:justify-start">
+<div class="lg:col-span-4 relative reveal flex justify-center lg:justify-start">
     <div class="w-64 h-80 sm:w-72 sm:h-[350px] rounded-3xl overflow-hidden relative group shadow-2xl">
         
         <img src="https://lh3.googleusercontent.com/d/1LyWIlqpaUQicual5SOnxfujYGxiIYjq1" 
